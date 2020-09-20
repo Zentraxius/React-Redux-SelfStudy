752638afc619386faab31bd78d5f894e4ddbb747
Cody Fritz <clanalia55@gmail.com>
2020-09-20 11:36:14 -0700

    save progress prior to implementation of react devtools using chrome

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/postActions';
 
@@ -7,7 +8,7 @@ componentWillMount(){
   this.props.fetchPosts();
 }
   render() {
-    const postItems = this.state.posts.map(post => (
+    const postItems = this.props.posts.map(post => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
@@ -21,5 +22,13 @@ componentWillMount(){
     )
   }
 }
+Posts.propTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired
+}
 
-export default connect(null,{fetchPosts})(Posts)
+const mapStateToProps = state => ({
+  posts: state.postReducerIndex.items
+});
+
+export default connect(mapStateToProps,{fetchPosts})(Posts)
