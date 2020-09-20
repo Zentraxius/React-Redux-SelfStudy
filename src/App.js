6b4fcef9b2d080291cbeca3399d9e2c26c4aa1bf
Cody Fritz <clanalia55@gmail.com>
2020-09-20 11:25:16 -0700

    save progress towards redux implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import Posts from "./components/Posts";
-import PostForm from "./components/Postform"
+import PostForm from "./components/Postform";
 import "./App.css";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import store from './components/store';
+// npm install redux react-redux redux-thunk
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <PostForm />
-        <hr />
-        <Posts />
-      </header>
-    </div>
-  );
+// const store = createStore(() => [], {}, applyMiddleware());
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <PostForm />
+            <hr />
+            <Posts />
+          </header>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
